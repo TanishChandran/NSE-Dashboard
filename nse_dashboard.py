b97fa67bbda35176a73789ad7771cd966deea642
Tanish Chandran <94378318+TanishChandran@users.noreply.github.com>
2024-11-08 22:38:59 +0530

    Update nse_dashboard.py

--- nse_dashboard.py
+++ nse_dashboard.py
@@ -1,3 +1,11 @@
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install(nselib)
+
 import nselib
 from nselib import capital_market
 from nselib import derivatives
